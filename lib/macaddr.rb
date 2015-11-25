d1fd138724bed9c1af55d1e6b3203d810b7b0545
pawel.cisek <pawel.cisek@puzzleflow.com>
2015-11-25 08:17:02 +0100

    null pfamily problem resolved

--- lib/macaddr.rb
+++ lib/macaddr.rb
@@ -83,7 +83,7 @@ module Mac
       return unless Socket.respond_to? :getifaddrs
 
       interfaces = Socket.getifaddrs.select do |addr|
-        addr.addr.pfamily == INTERFACE_PACKET_FAMILY
+        addr.addr.pfamily == INTERFACE_PACKET_FAMILY if addr.addr.try(:pfamily)
       end
 
       mac, =
